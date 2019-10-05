0d754cc174184d64be1260b5aa526262c3b6bec3
Rick Yiu <rickyiu@google.com>
2019-10-05 15:59:06 +0800

    sched: use sysctl to control energy aware feature
    Energy aware feature control is previously done through debugfs,
    which will be deprecated, so move the control to sysctl.
    
    Bug: 141333728
    Test: function works as expected
    Change-Id: I55411d3bb2669ba1fae3225d67cdf1cf8b3b3a7f
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    idkwhoiam322: Fix minor merge conflicts.
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -137,6 +137,8 @@ extern void init_sched_groups_capacity(int cpu, struct sched_domain *sd);
 static inline void cpu_load_update_active(struct rq *this_rq) { }
 #endif
 
+extern bool energy_aware(void);
+
 /*
  * Helpers for converting nanosecond timing to jiffy resolution
  */
@@ -3125,9 +3127,27 @@ static inline void walt_map_freq_to_load(void) { }
 static inline void walt_update_min_max_capacity(void) { }
 #endif	/* CONFIG_SCHED_WALT */
 
-static inline bool energy_aware(void)
+#define	CPU_RESERVED	1
+
+static inline int is_reserved(int cpu)
 {
-	return sched_feat(ENERGY_AWARE);
+	struct rq *rq = cpu_rq(cpu);
+
+	return test_bit(CPU_RESERVED, &rq->extra_flags);
+}
+
+static inline int mark_reserved(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	return test_and_set_bit(CPU_RESERVED, &rq->extra_flags);
+}
+
+static inline void clear_reserved(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	clear_bit(CPU_RESERVED, &rq->extra_flags);
 }
 
 struct sched_avg_stats {
