474b7f45e99454c7bbbb86d1862b58a234c23557
henloboi <henloboii@protonmail.ch>
2020-09-09 18:46:29 +0800

    keyreset : do nothing

--- drivers/input/keyreset.c
+++ drivers/input/keyreset.c
@@ -22,17 +22,17 @@
 #include <linux/slab.h>
 #include <linux/syscalls.h>
 #include <linux/keycombo.h>
-#include <linux/of.h>
 
 struct keyreset_state {
 	int restart_requested;
+	int (*reset_fn)(void);
 	struct platform_device *pdev_child;
 	struct work_struct restart_work;
 };
 
 static void do_restart(struct work_struct *unused)
 {
-	machine_restart("recovery");
+	orderly_reboot();
 }
 
 static void do_reset_fn(void *priv)
@@ -40,21 +40,26 @@ static void do_reset_fn(void *priv)
 	struct keyreset_state *state = priv;
 	if (state->restart_requested)
 		panic("keyboard reset failed, %d", state->restart_requested);
-
-	schedule_work(&state->restart_work);
-	state->restart_requested = 1;
+	if (state->reset_fn) {
+		state->restart_requested = state->reset_fn();
+	} else {
+		pr_info("keyboard reset\n");
+		schedule_work(&state->restart_work);
+		state->restart_requested = 1;
+	}
 }
 
 static int keyreset_probe(struct platform_device *pdev)
 {
-	struct keycombo_platform_data *pdata_child;
-	struct device_node *np = pdev->dev.of_node;
-	struct keyreset_state *state;
-	struct property *prop;
-	int key_count = 0, size, key, i;
-	int *keys, *val;
 	int ret = -ENOMEM;
+	struct keycombo_platform_data *pdata_child;
+	struct keyreset_platform_data *pdata = pdev->dev.platform_data;
+	int up_size = 0, down_size = 0, size;
+	int key, *keyp;
+	struct keyreset_state *state;
 
+	if (!pdata)
+		return -EINVAL;
 	state = devm_kzalloc(&pdev->dev, sizeof(*state), GFP_KERNEL);
 	if (!state)
 		return -ENOMEM;
@@ -63,43 +68,47 @@ static int keyreset_probe(struct platform_device *pdev)
 							PLATFORM_DEVID_AUTO);
 	if (!state->pdev_child)
 		return -ENOMEM;
-
 	state->pdev_child->dev.parent = &pdev->dev;
 	INIT_WORK(&state->restart_work, do_restart);
 
-	prop = of_find_property(np, "keys-down", NULL);
-	if (!prop)
-		return -ENOMEM;
-
-	key_count = prop->length / sizeof(u32);
-	keys = devm_kzalloc(&pdev->dev, key_count * sizeof(int), GFP_KERNEL);
-	if (!keys)
-		return -ENOMEM;
-
-	val = prop->value;
-	for (i = 0; i < key_count; i++) {
-		key = (unsigned short) be32_to_cpup(val++);
-		if (key == KEY_RESERVED || key >= KEY_MAX)
+	keyp = pdata->keys_down;
+	while ((key = *keyp++)) {
+		if (key >= KEY_MAX)
 			continue;
-
-		keys[i] = key;
+		down_size++;
 	}
-
-	size = sizeof(struct keycombo_platform_data) + sizeof(int) * (key_count + 1);
+	if (pdata->keys_up) {
+		keyp = pdata->keys_up;
+		while ((key = *keyp++)) {
+			if (key >= KEY_MAX)
+				continue;
+			up_size++;
+		}
+	}
+	size = sizeof(struct keycombo_platform_data)
+			+ sizeof(int) * (down_size + 1);
 	pdata_child = devm_kzalloc(&pdev->dev, size, GFP_KERNEL);
 	if (!pdata_child)
 		goto error;
-
-	memcpy(pdata_child->keys_down, keys,
-			sizeof(int) * key_count);
-
+	memcpy(pdata_child->keys_down, pdata->keys_down,
+						sizeof(int) * down_size);
+	if (up_size > 0) {
+		pdata_child->keys_up = devm_kzalloc(&pdev->dev, up_size + 1,
+								GFP_KERNEL);
+		if (!pdata_child->keys_up)
+			goto error;
+		memcpy(pdata_child->keys_up, pdata->keys_up,
+							sizeof(int) * up_size);
+		if (!pdata_child->keys_up)
+			goto error;
+	}
+	state->reset_fn = pdata->reset_fn;
 	pdata_child->key_down_fn = do_reset_fn;
 	pdata_child->priv = state;
-	pdata_child->key_down_delay = 0;
+	pdata_child->key_down_delay = pdata->key_down_delay;
 	ret = platform_device_add_data(state->pdev_child, pdata_child, size);
 	if (ret)
 		goto error;
-
 	platform_set_drvdata(pdev, state);
 	return platform_device_add(state->pdev_child);
 error:
@@ -114,20 +123,22 @@ int keyreset_remove(struct platform_device *pdev)
 	return 0;
 }
 
-static struct of_device_id keyreset_dt_match[] = {
-	{ .compatible = "android,keyreset", },
-	{ },
-};
-MODULE_DEVICE_TABLE(of, keyreset_dt_match);
 
 struct platform_driver keyreset_driver = {
+	.driver.name = KEYRESET_NAME,
 	.probe = keyreset_probe,
 	.remove = keyreset_remove,
-	.driver = {
-		.name	= KEYRESET_NAME,
-		.of_match_table = of_match_ptr(keyreset_dt_match),
-		.owner	= THIS_MODULE,
-	},
 };
 
-module_platform_driver(keyreset_driver);
+static int __init keyreset_init(void)
+{
+	return platform_driver_register(&keyreset_driver);
+}
+
+static void __exit keyreset_exit(void)
+{
+	return platform_driver_unregister(&keyreset_driver);
+}
+
+module_init(keyreset_init);
+module_exit(keyreset_exit);
